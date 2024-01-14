f8e823a4dc9b113f2ee8d85b256759b3ee48357e
Stelian Fedorca <stelian.fedorca25@gmail.com>
2024-01-14 13:15:34 +0200

    added select compoent

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,55 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper value={value} onChange={onChange}>
+      <NativeSelect>{children}</NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down" size={24} strokeWidth={1} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const IconWrapper = styled.div`
+  color: inherit;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+`;
+
+const PresentationalBit = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  border-radius: 8px;
+  font-size: 1rem;
+  color: ${COLORS.gray700};
+  font-family: 'Roboto', sans-serif;
+
+  ${NativeSelect}:focus + & {
+    outline: 2px solid #4374cb;
+  }
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
